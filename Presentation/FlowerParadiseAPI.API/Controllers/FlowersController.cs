53767d0517b2e1228cac4a9bc9746ff6eb2bba2d
emineegnss <80625362+emineegnss@users.noreply.github.com>
2023-11-17 22:31:27 +0300

    17/11 Commit 1

--- Presentation/FlowerParadiseAPI.API/Controllers/FlowersController.cs
+++ Presentation/FlowerParadiseAPI.API/Controllers/FlowersController.cs
@@ -1,7 +1,9 @@
 ﻿using FlowerParadiseAPI.Application.Repositories;
+using FlowerParadiseAPI.Application.ViewModels.Flowers;
 using FlowerParadiseAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using static System.Net.WebRequestMethods;
 
 namespace FlowerParadiseAPI.API.Controllers
@@ -11,15 +13,13 @@ namespace FlowerParadiseAPI.API.Controllers
     public class FlowersController : ControllerBase
     {
         private readonly IFlowerRepository _flowerRepository;
-        private readonly IFlowerSpeciesRepository _flowerSpeciesRepository;
 
         public FlowersController(IFlowerRepository flowerRepository, IFlowerSpeciesRepository flowerSpeciesRepository)
         {
             _flowerRepository = flowerRepository;
-            _flowerSpeciesRepository = flowerSpeciesRepository;
         }
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> Get()
         {
             //var speciesId = Guid.NewGuid();
             //await _flowerSpeciesRepository.AddAsync(new() { Id = speciesId, SpeciesName = "Gül", CreateDate = DateTime.UtcNow });
@@ -27,10 +27,27 @@ namespace FlowerParadiseAPI.API.Controllers
             //await _flowerRepository.AddAsync(new() { Name = "Gül 1", Price = 12, Stock = 15,CreateDate=DateTime.UtcNow,SpeciesId=speciesId}) ;
 
             // await _flowerSpeciesRepository.SaveAsync();
-            Flower flower = await _flowerRepository.GetByIdAsync("6330b86e-a91b-4647-b0d5-ba66e4072089");
-            flower.Name = "Gül 2";
-            _flowerRepository.SaveAsync();
-            return Ok("Hi!");
+            //Flower flower = await _flowerRepository.GetByIdAsync("6330b86e-a91b-4647-b0d5-ba66e4072089");
+            //flower.Name = "Gül 2";
+            //_flowerRepository.SaveAsync();
+            return Ok(_flowerRepository.GetAll());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(VM_Create_Flower model)
+        {
+           await _flowerRepository.AddAsync(new()
+            {
+                Name = model.Name,
+                Stock = model.Stock,
+                Price = model.Price,
+            });
+            await _flowerRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put()
+        {
+
         }
     }
 }
