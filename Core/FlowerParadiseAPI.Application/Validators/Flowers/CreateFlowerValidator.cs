c77aac09e75e70d8410a99be5a0550bccb5cfc2a
emineegnss <80625362+emineegnss@users.noreply.github.com>
2023-11-27 22:52:19 +0300

    27/11 Commit/1

---
+++ Core/FlowerParadiseAPI.Application/Validators/Flowers/CreateFlowerValidator.cs
@@ -0,0 +1,14 @@
+﻿using FlowerParadiseAPI.Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerParadiseAPI.Application.Validators.Flowers
+{
+    public class CreateFlowerValidator : AbstractValidator<Flower>
+    {
+    }
+}
